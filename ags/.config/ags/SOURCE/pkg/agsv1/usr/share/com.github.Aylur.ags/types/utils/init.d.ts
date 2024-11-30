3ad8e83fe0c1ce7bd6909aa6c0d253563d904502
andrwui <moralesocampoandres@gmail.com>
2024-11-30 12:41:58 -0300

    accidentally added ALL ags source files, oops

--- ags/.config/ags/SOURCE/pkg/agsv1/usr/share/com.github.Aylur.ags/types/utils/init.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isRunning(dbusName: string, bus: 'session' | 'system'): boolean;
-export declare function parsePath(path: string): string;
-export declare function init(configDir: string, entry: string): Promise<void>;
