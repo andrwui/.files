021d2cdbcb1e7bd7aea6a86858e29bbde3c0cfda
andrwui <moralesocampoandres@gmail.com>
2025-02-24 22:34:54 -0300

    changed ssm and added swm for tmux

--- tmux/tmux-ssm.sh
+++ tmux/tmux-ssm.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-
-# List all tmux sessions
-SESSIONS=$(tmux list-sessions -F "#{session_name}")
-session_list=($SESSIONS)
-session_count=${#session_list[@]}
-
-# Adjust popup height based on session count
-popup_height=$((session_count + 4))
-
-# Open the tmux popup
-tmux popup -E -w 15% -h $popup_height -d '#{pane_current_path}' "bash -c '~/.config/tmux/tmux-ssm-select.sh'"
-
-
+POPUP_WIDTH="20%"
+POPUP_HEIGHT="30%"
+TMPFILE=$(mktemp /tmp/tmux-sessions-fzf.XXXXXX)
+SESSIONS=$(tmux list-sessions -F '#{session_name}')
+if [ -z "$SESSIONS" ]; then
+  echo "No tmux sessions found."
+  exit 0
+fi
+tmux popup -E -w "$POPUP_WIDTH" -h "$POPUP_HEIGHT" -d '#{pane_current_path}' "
+  tmux list-sessions -F '#{session_name}#{?session_attached, <,}' | 
+  fzf --reverse --height=100% --border=none --prompt='session > ' \\
+      --color=bw \\
+      --expect=ctrl-r,ctrl-k \\
+      --bind 'esc:abort' > $TMPFILE
+"
+if [ -s "$TMPFILE" ]; then
+  KEY=$(head -1 "$TMPFILE")
+  SELECTION=$(head -2 "$TMPFILE" | tail -1 | sed 's/ <$//')
+  if [ "$KEY" = "ctrl-r" ] && [ -n "$SELECTION" ]; then
+    tmux command-prompt -p "New name:" -I "$SELECTION" "rename-session -t \"$SELECTION\" '%%'"
+  elif [ "$KEY" = "ctrl-k" ] && [ -n "$SELECTION" ]; then
+    tmux kill-session -t "$SELECTION"
+  else
+    tmux switch-client -t "$SELECTION"
+  fi
+fi
+rm -f "$TMPFILE"
+exit 0
